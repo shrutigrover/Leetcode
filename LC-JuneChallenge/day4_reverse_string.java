9eeaaed2130dd2064dcf91cc1d70ef3b90fe8ee0
shrutigrover <shrutigrover693@gmail.com>
2020-06-04 15:20:48 -0400

    June Challenge - Day 4 - Reverse String

---
+++ LC-JuneChallenge/day4_reverse_string.java
@@ -0,0 +1,26 @@
+/**
+Problem - Reverse String
+Write a function that reverses a string. The input string is given as an array of characters char[]. Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory. You may assume all the characters consist of printable ascii characters.
+
+Example - 
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+**/
+/* Solution - Using Two Pointers */
+class Solution {
+    public void reverseString(char[] s) {
+        
+        int i = 0;
+        int j = s.length - 1;
+        
+        while(i <= j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            
+            i++;
+            j--;
+        }
+    }
+}
